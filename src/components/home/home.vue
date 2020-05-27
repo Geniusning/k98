1f20b6fd3067bf7f588dbe4bcdb7cd808ee1a96c
Geniusning <690984649@qq.com>
2020-05-27 09:29:55 +0800

    修改首页进入游戏链接

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -518,10 +518,10 @@ export default {
       console.log("home-identity--",identity_h)
       if(identity_h){
         let isMasterId = identity_h.indexOf("@master")
-        identity_h = isMasterId > 0 ? identity_h : "" 
+        identity_h = isMasterId > 0 ? identity_h : this.userInfo.openid
       }
       window.location.href = `${this.gameUrl}game?identity=${identity_h}`;
-     
+
     },
     gotoFriend() {
       // util.routerTo("friend", this, {
